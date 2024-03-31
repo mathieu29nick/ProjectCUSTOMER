3d9f7fde33166baf47cba1a0af92eb2c55e6f3f3
Nick Mathieu <rakotomanananick29@gmail.com>
2024-03-31 18:20:00 +0300

    Ajout d'un bean CDI pour gérer les données de la base sur les codes de réduction

---
+++ src/main/java/com/mycompany/projectcustomer/service/DiscountManager.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectcustomer.service;
+
+import com.mycompany.projectcustomer.entity.Discount;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Façade pour gérer les Discount_Code
+ *
+ * @author rakot
+ */
+@RequestScoped
+public class DiscountManager {
+
+    @PersistenceContext(unitName = "discountPU")
+    private EntityManager dm;
+
+    @Transactional
+    public void persist(Discount discount) {
+        dm.persist(discount);
+    }
+
+    public List<Discount> getAllCustomers() {
+        Query query = dm.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return dm.find(Discount.class, code);
+    }
+}
